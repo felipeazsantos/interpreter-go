09c31efe801855665416e75c4cdac0202b8da7d5
felipeazsantos <felipeasantos10@hotmail.com>
2026-01-15 17:29:20 -0300

    printing token of monkey programming language

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/felipeazsantos/interpreter-go/repl"
+)
 
 func main() {
-	fmt.Println('Z')
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey Programming Language!\n", user.Username)
+	fmt.Printf("Feel free to type in the program / command\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
